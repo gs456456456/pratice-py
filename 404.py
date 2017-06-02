059de94eeeee50ef69ed50d2ce96b8e437684043
gs456456 <1154442437@qq.com>
2017-06-02 10:33:37 +0800

    404

---
+++ 404.py
@@ -0,0 +1,55 @@
+#-*- coding:utf-8 -*-
+import sys, os, BaseHTTPServer
+
+class ServerException(Exception):
+    pass
+
+class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+
+    Error_Page = """\
+    <html>
+    <body>
+    <h1>Error accessing {path}</h1>
+    <p>{msg}</p>
+    </body>
+    </html>
+    """
+
+    def do_GET(self):
+        try:
+            full_path = os.getcwd() + self.path
+            if not os.path.exists(full_path):
+                print(full_path)
+                raise ServerException("'{0}' not found".format(self.path))
+            elif os.path.isfile(full_path):
+                self.handle_file(full_path)
+            else:
+                raise ServerException("Unknown object '{0}'".format(self.path))
+
+        except Exception as msg:
+            self.handle_error(msg)
+
+    def handle_error(self, msg):
+        content = self.Error_Page.format(path=self.path, msg=msg)
+        self.send_content(content, 404)
+
+    def send_content(self, content, status=200):
+        self.send_response(status)
+        self.send_header("Content-type", "text/html")
+        self.send_header("Content-Length", str(len(content)))
+        self.end_headers()
+        self.wfile.write(content)
+
+    def handle_file(self, full_path):
+        try:
+            with open(full_path, 'rb') as reader:
+                content = reader.read()
+            self.send_content(content)
+        except IOError as msg:
+            msg = "'{0}' cannot be read: {1}".format(self.path, msg)
+            self.handle_error(msg)
+
+if __name__ == '__main__':
+    serverAddress = ('', 8080)
+    server = BaseHTTPServer.HTTPServer(serverAddress, RequestHandler)
+    server.serve_forever()
--- messageboard/mysite/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from mysite import models
-
-class PostAdmin(admin.ModelAdmin):
-    list_display=('nickname', 'message', 'enabled', 'pub_time')
-    ordering=('-pub_time',)
-admin.site.register(models.Mood)
-admin.site.register(models.Post, PostAdmin)
-
